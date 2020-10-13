4531d673b86cfe0f9e40e7b40b6a1528f4e99d86
sbabcock23 <steve.w.babcock@gmail.com>
2020-10-13 17:49:07 -0400

    updated setup file

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+if sys.argv[-1] == 'compile':
+    os.system('python setup.py bdist_wheel')
     sys.exit()
 
 # read the contents of your README file
@@ -13,6 +13,9 @@ from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+pkg = find_packages("pytryfi")
+print(pkg)
 
 setup(
     name="pytryfi", # Replace with your own username
@@ -23,7 +26,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sbabcock23/pytryfi",
-    packages=find_packages(),
+    packages={'pytryfi', 'pytryfi/common'},
     include_package_data=True,
     classifiers=[ "Programming Language :: Python :: 3",
                     "License :: OSI Approved :: Apache Software License",
